49641347795cb4427505816260d164d0e1a4d059
Matteo Fenu <matfen97@gmail.com>
2020-08-03 13:51:17 +0200

    epic build

---
+++ client/build/precache-manifest.88d64d5852f4eab29fa486709f8de491.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7422b915ac4fba984a0dc4767d8151d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a7a84fc5ac90e48738d",
+    "url": "/static/css/main.dbb077e7.chunk.css"
+  },
+  {
+    "revision": "46eb62b2ba2ecf21fcec",
+    "url": "/static/js/2.f6a83548.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f6a83548.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a7a84fc5ac90e48738d",
+    "url": "/static/js/main.623eb50d.chunk.js"
+  },
+  {
+    "revision": "1eede93b43bca28572ca",
+    "url": "/static/js/runtime-main.399baf1c.js"
+  }
+]);
