9adcb5e8b55a53002b70e84a47b544c33e171075
Matteo Fenu <matfen97@gmail.com>
2020-08-03 11:19:07 +0200

    make it better

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect("mongodb+srv://mawmawtee:n4KG0M9NDqyWSajW@cluster0-jzimm.mongodb.net/exprensetracker?retryWrites=true&w=majority", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
