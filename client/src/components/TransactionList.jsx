6752abdf7cb6d57fd888ee60353991d2643b7bac
Matteo Fenu <matfen97@gmail.com>
2020-08-03 12:34:27 +0200

    fixed

--- client/src/components/TransactionList.jsx
+++ client/src/components/TransactionList.jsx
@@ -5,7 +5,7 @@ export const TransactionList = () => {
     const { transactions, getTransactions } = useContext(GlobalContext)
     useEffect(() => {
         getTransactions()
-        // eslint-disable-next-line react-hooks/exhaustive-deps
+        //  eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     return (
         <>
